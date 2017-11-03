c5c575df202618f528c9a7edb4a7dde2278d9fdd
Eric Johnson <0216johnsoneric@gmail.com>
2017-11-03 16:33:18 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,7 +38,7 @@ end
 def play(board)
 	     counter = 0
 	     until counter == 9
-	        puts "Wingardium Leviosa"
+	        puts "This is the #{counter} turn."
 	        counter += 1
 	    end
 end
